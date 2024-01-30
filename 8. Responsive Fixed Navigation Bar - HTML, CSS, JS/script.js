3548a35eea56e950d3ef053dc58735f7a3999f20
SQCodes <paulsqmagadi@gmail.com>
2024-01-30 15:00:02 +0300

    File update

--- 8. Responsive Fixed Navigation Bar - HTML, CSS, JS/script.js
+++ 8. Responsive Fixed Navigation Bar - HTML, CSS, JS/script.js
@@ -14,6 +14,50 @@ toggleButton.addEventListener('click', () => {
 });
 
 
+const loginFormButton = document.getElementById("login-button");
+const signinForm = document.getElementById("signin-form");
+const closeModal = document.getElementById("close");
+const cancel = document.getElementById("cancelbtn");
+
+loginFormButton.addEventListener("click", () => {
+    signinForm.style.display = "block";
+})
+
+closeModal.addEventListener("click", () => {
+    signinForm.style.display = "none";
+})
+
+cancel.addEventListener("click", () => {
+    signinForm.style.display = "none";
+})
+
+// Registration form
+const signupFormButton = document.getElementById("signup-button");
+const signupForm = document.getElementById("signup-form");
+const closeSignupForm = document.getElementById("close-signup-form");
+const cancelSignup = document.getElementById("cancel-signup-button");
+
+signupFormButton.addEventListener("click", () => {
+    signupForm.style.display = "block";
+})
+
+closeSignupForm.addEventListener("click", () => {
+    signupForm.style.display = "none";
+})
+
+cancelSignup.addEventListener("click", () => {
+    signupForm.style.display = "none";
+})
+
+// close the login and signup form when a user clicks outside the forms
+window.onclick = (event) =>{
+    if (event.target == signupForm || event.target == signinForm) {
+        signupForm.style.display = "none";
+        signinForm.style.display = "none";
+    }
+}
+
+
 
 
 
