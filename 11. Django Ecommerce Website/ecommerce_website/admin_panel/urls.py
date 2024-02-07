d07f96c53405c688447c278afe231c5186994959
SQCodes <paulsqmagadi@gmail.com>
2024-02-07 15:54:51 +0300

    modified:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__pycache__/urls.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__pycache__/views.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/admin_panel/templates/dashboard.html 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/templates/home.html 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/templates/master.html 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/templates/nav.html 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/templates/signin.html 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/templates/signup.html 	modified:   11. Django Ecommerce Website/ecommerce_website/admin_panel/urls.py 	modified:   11. Django Ecommerce Website/ecommerce_website/admin_panel/views.py 	modified:   11. Django Ecommerce Website/ecommerce_website/db.sqlite3 	new file:   11. Django Ecommerce Website/ecommerce_website/static/css/admin/style.css 	modified:   11. Django Ecommerce Website/ecommerce_website/store/__pycache__/views.cpython-311.pyc

--- 11. Django Ecommerce Website/ecommerce_website/admin_panel/urls.py
+++ 11. Django Ecommerce Website/ecommerce_website/admin_panel/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('signin/', views.admin_signin, name='signin'),
+    path('signout/', views.admin_signout, name='signout'),
+    path('signup/', views.admin_signup, name='signup'),
+    
 ]
--- 11. Django Ecommerce Website/ecommerce_website/admin_panel/views.py
+++ 11. Django Ecommerce Website/ecommerce_website/admin_panel/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def dashboard(request):
     return render(request, 'dashboard.html')
+
+def admin_signin(request):
+    return render(request, 'signin.html')
+
+# User Login    
+def admin_signin(request):
+    if request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user is not None: 
+                login(request, user)
+                messages.success(request, ('Login sucess!'))
+                return redirect('dashboard')
+            else:
+                messages.success(request, ('Error Logging in. Try again!!!'))
+                return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+    
+# User Logout  
+def admin_signout(request):
+    logout(request)
+    messages.success(request, ('You have been logged out!!!'))
+    return redirect('signin')
+
+def admin_signup(request):
+    return render(request, 'signup.html')
