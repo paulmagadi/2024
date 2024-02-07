53f3bf0587a327fdca7c0da408693d9562be6c71
SQCodes <paulsqmagadi@gmail.com>
2024-02-07 09:42:09 +0300

    new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__init__.py 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__pycache__/__init__.cpython-311.pyc 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__pycache__/admin.cpython-311.pyc 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__pycache__/apps.cpython-311.pyc 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__pycache__/models.cpython-311.pyc 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__pycache__/urls.cpython-311.pyc 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/__pycache__/views.cpython-311.pyc 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/admin.py 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/apps.py 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/migrations/__init__.py 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/migrations/__pycache__/__init__.cpython-311.pyc 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/models.py 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/templates/dashboard.html 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/tests.py 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/urls.py 	new file:   11. Django Ecommerce Website/ecommerce_website/admin_panel/views.py 	modified:   11. Django Ecommerce Website/ecommerce_website/ecommerce_website/__pycache__/settings.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/ecommerce_website/settings.py 	modified:   11. Django Ecommerce Website/ecommerce_website/ecommerce_website/urls.py

--- 11. Django Ecommerce Website/ecommerce_website/ecommerce_website/settings.py
+++ 11. Django Ecommerce Website/ecommerce_website/ecommerce_website/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store',
+    'admin_panel'
 ]
 
 MIDDLEWARE = [
--- 11. Django Ecommerce Website/ecommerce_website/ecommerce_website/urls.py
+++ 11. Django Ecommerce Website/ecommerce_website/ecommerce_website/urls.py
@@ -24,4 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('', include('admin_panel.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
