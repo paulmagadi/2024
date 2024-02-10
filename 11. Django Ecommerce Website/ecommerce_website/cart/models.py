8eba08b818cb3dfc5c98c0b392fc16bb09cfff12
SQCodes <paulsqmagadi@gmail.com>
2024-02-10 14:46:21 +0300

    modified:   11. Django Ecommerce Website/ecommerce_website/cart/__pycache__/admin.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/cart/__pycache__/cart.cpython-311.pyc 	new file:   11. Django Ecommerce Website/ecommerce_website/cart/__pycache__/context_processors.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/cart/__pycache__/models.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/cart/admin.py 	modified:   11. Django Ecommerce Website/ecommerce_website/cart/cart.py 	new file:   11. Django Ecommerce Website/ecommerce_website/cart/migrations/0001_initial.py 	new file:   11. Django Ecommerce Website/ecommerce_website/cart/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/cart/models.py 	modified:   11. Django Ecommerce Website/ecommerce_website/db.sqlite3 	modified:   11. Django Ecommerce Website/ecommerce_website/ecommerce_website/__pycache__/settings.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   11. Django Ecommerce Website/ecommerce_website/ecommerce_website/settings.py 	modified:   11. Django Ecommerce Website/ecommerce_website/ecommerce_website/urls.py 	modified:   11. Django Ecommerce Website/ecommerce_website/static/js/main.js 	modified:   11. Django Ecommerce Website/ecommerce_website/store/templates/store/include/card.html

--- 11. Django Ecommerce Website/ecommerce_website/cart/models.py
+++ 11. Django Ecommerce Website/ecommerce_website/cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from store.models import Product, Customer
 
 # Create your models here.
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
--- 11. Django Ecommerce Website/ecommerce_website/static/js/main.js
+++ 11. Django Ecommerce Website/ecommerce_website/static/js/main.js
@@ -58,14 +58,8 @@ cancelSignup.addEventListener("click", () => {
 //     }
 // }
 
-// Footer
 
-// const currentYear = document.getElementById('current-year');
 
-// let date = new Date();
-// let newDate = date.getFullYear();
-
-// currentYear.innerHTML = newDate;
 
 
 
