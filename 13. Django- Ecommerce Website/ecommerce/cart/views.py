78ffbcb1150f2fad50c9fe3a281e9e86cca52d50
SQCodes <paulsqmagadi@gmail.com>
2024-02-13 18:29:29 +0300

    new file:   13. Django- Ecommerce Website/ecommerce/cart/__pycache__/__init__.cpython-311.pyc 	renamed:    13. Django- Ecommerce Website/ecommerce/store/templates/store/cart.html -> 13. Django- Ecommerce Website/ecommerce/cart/templates/cart/cart.html 	renamed:    13. Django- Ecommerce Website/ecommerce/store/templates/store/checkout.html -> 13. Django- Ecommerce Website/ecommerce/cart/templates/cart/checkout.html 	new file:   13. Django- Ecommerce Website/ecommerce/cart/urls.py 	modified:   13. Django- Ecommerce Website/ecommerce/cart/views.py 	modified:   13. Django- Ecommerce Website/ecommerce/ecommerce/__pycache__/urls.cpython-311.pyc 	modified:   13. Django- Ecommerce Website/ecommerce/ecommerce/urls.py 	modified:   13. Django- Ecommerce Website/ecommerce/store/templates/store/base.html 	modified:   13. Django- Ecommerce Website/ecommerce/store/templates/store/login.html 	modified:   13. Django- Ecommerce Website/ecommerce/store/templates/store/register.html

--- 13. Django- Ecommerce Website/ecommerce/cart/views.py
+++ 13. Django- Ecommerce Website/ecommerce/cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cart(request):
+    return render(request, 'cart/cart.html')
+
+def chechout(request):
+    return render(request, 'cart/checkout.html')
--- 13. Django- Ecommerce Website/ecommerce/ecommerce/urls.py
+++ 13. Django- Ecommerce Website/ecommerce/ecommerce/urls.py
@@ -23,4 +23,5 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('', include('cart.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
