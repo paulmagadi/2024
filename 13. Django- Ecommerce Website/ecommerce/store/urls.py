793024d33981287d0efc6cffd496578a495b949a
SQCodes <paulsqmagadi@gmail.com>
2024-02-15 17:36:12 +0300

    modified:   13. Django- Ecommerce Website/ecommerce/store/__pycache__/urls.cpython-312.pyc 	modified:   13. Django- Ecommerce Website/ecommerce/store/urls.py

--- 13. Django- Ecommerce Website/ecommerce/store/urls.py
+++ 13. Django- Ecommerce Website/ecommerce/store/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout', views.user_logout, name='logout'),
     path('new_products', views.new_products, name='new_products'),
+    path('sale', views.sale_products, name='sale'),
+    path('featured', views.featured_products, name='featured'),
 ]
