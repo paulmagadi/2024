070f2c90aa997e82a3385f2568a9b0536fe6e948
SQCodes <paulsqmagadi@gmail.com>
2024-02-19 18:31:10 +0300

    modified:   13. Django- Ecommerce Website/ecommerce/cart/__pycache__/urls.cpython-311.pyc 	modified:   13. Django- Ecommerce Website/ecommerce/cart/urls.py

--- 13. Django- Ecommerce Website/ecommerce/cart/urls.py
+++ 13. Django- Ecommerce Website/ecommerce/cart/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
-    path('cart_add', views.cart_add, name='cart_add'),
+    path('cart_add/', views.cart_add, name='cart_add'),
 ]
