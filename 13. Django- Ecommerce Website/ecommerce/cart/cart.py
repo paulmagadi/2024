afa72a7b63ed2949941eaf4f24e34c5b64569553
SQCodes <paulsqmagadi@gmail.com>
2024-02-19 17:32:10 +0300

    new file:   13. Django- Ecommerce Website/ecommerce/cart/cart.py 	new file:   13. Django- Ecommerce Website/ecommerce/cart/context_processors.py 	modified:   13. Django- Ecommerce Website/ecommerce/ecommerce/settings.py

---
+++ 13. Django- Ecommerce Website/ecommerce/cart/cart.py
@@ -0,0 +1,12 @@
+from store.models import Product
+
+class Cart():
+    
+    def __init__(self, request):
+        self.session = request.session
+        cart =  self.session.get('session_key')
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+            self.cart = cart
+        
+    
