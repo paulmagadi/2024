c2d8d597cce1922f758f0048a6f13b22a7b0ad35
SQCodes <paulsqmagadi@gmail.com>
2024-02-23 10:17:17 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/cart/__pycache__/models.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/cart/models.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/__pycache__/models.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/models.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'categories'
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(default=0, max_digits=9, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='uploads/products/')
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=8, null=True, blank=True)
+    in_stock = models.BooleanField(default=True)
+    stock_quantity = models.IntegerField(default=1)
+    discount = models.DecimalField(default=0, max_digits=9, decimal_places=2, null=True, blank=True)
+    percentage_discount = models.DecimalField(default=0, max_digits=5, decimal_places=0, null=True, blank=True)
+    is_new = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False)
+    
+
+    def save(self, *args, **kwargs):
+        if self.is_sale and self.sale_price < self.price:
+            self.discount = round(self.price - self.sale_price, 2)
+            self.percentage_discount = round((self.discount / self.price) * 100)
+        else:
+            self.discount = 0
+            self.percentage_discount = 0
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
