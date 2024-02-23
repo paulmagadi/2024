4c8de9290b3aa1cf88acf8a00afbb44e3087dc16
SQCodes <paulsqmagadi@gmail.com>
2024-02-23 11:23:04 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/db.sqlite3 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/__pycache__/models.cpython-311.pyc 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/store/migrations/0002_category_parent_category.py 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/store/migrations/0003_remove_product_category_product_categories.py 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/store/migrations/__pycache__/0002_category_parent_category.cpython-311.pyc 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/store/migrations/__pycache__/0003_remove_product_category_product_categories.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/models.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
@@ -5,8 +5,6 @@ import datetime
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
-
     
     def __str__(self):
         return self.name
