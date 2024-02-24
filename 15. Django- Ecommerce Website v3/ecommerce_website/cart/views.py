32775bd9da38fb29a62bb84a1bb6901c71606d84
SQCodes <paulsqmagadi@gmail.com>
2024-02-24 17:05:16 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/cart/__pycache__/cart.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/cart/__pycache__/views.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/cart/views.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/cart/views.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 # Create your views here.
 def cart(request):
@@ -9,7 +12,15 @@ def checkout(request):
     return render(request, 'cart/checkout.html')
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('product_id'):
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
+        cart.add(product=product)
+        
+        cart_quantity = cart.__len__()
+        response = JsonResponse({'qty': cart_quantity})
+        return response
 
 def cart_delete(request):
     pass
