82b267be679f05928ba66f6a857697dd490781ea
SQCodes <paulsqmagadi@gmail.com>
2024-02-24 16:58:33 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/cart/cart.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/templates/store/base.html 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/templates/store/include/navbar.html

--- 15. Django- Ecommerce Website v3/ecommerce_website/cart/cart.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/cart/cart.py
@@ -6,3 +6,15 @@ class Cart():
             cart = request.session['session_key'] = {}
             
         self.cart = cart
+        
+    def add(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        self.session.modified = True
+        
+        
+    def __len__(self):
+        return len(self.cart)
