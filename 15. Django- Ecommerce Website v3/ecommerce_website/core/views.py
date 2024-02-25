c8111d8a5aac395da42fc56501da2d309b333196
SQCodes <paulsqmagadi@gmail.com>
2024-02-25 19:46:44 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/core/__pycache__/views.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/core/views.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/db.sqlite3

--- 15. Django- Ecommerce Website v3/ecommerce_website/core/views.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/core/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+# from .forms import CreateUserForm
+from django.contrib import messages
 
 
 def register_user(request):
