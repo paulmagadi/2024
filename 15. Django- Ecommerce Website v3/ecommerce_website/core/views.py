6de926e111a62fe6ff7edbe1d80bc796e7748f9d
SQCodes <paulsqmagadi@gmail.com>
2024-02-27 15:43:05 +0300

    new file:   15. Django- Ecommerce Website v3/ecommerce_website/core/__pycache__/forms.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/core/__pycache__/views.cpython-311.pyc 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/core/forms.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/core/templates/core/register.html 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/core/views.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/db.sqlite3

--- 15. Django- Ecommerce Website v3/ecommerce_website/core/views.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/core/views.py
@@ -2,10 +2,25 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 # from .forms import CreateUserForm
 from django.contrib import messages
+from .forms import RegistrationForm
+from django.contrib.auth.models import User
 
 
 def register_user(request):
-    return render(request, 'core/register.html')
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_name=last_name)
+            user.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm()
+    return render(request, 'core/register.html', {'form': form})
 
 def login_user(request):
     if request.method == "POST":
