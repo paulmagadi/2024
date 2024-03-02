5b66ab87a054abe63ee7ad03f41e7bd5403e2ba4
SQCodes <paulsqmagadi@gmail.com>
2024-03-02 12:46:30 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/cart/templates/cart/cart.html 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/core/__pycache__/views.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/core/views.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/db.sqlite3

--- 15. Django- Ecommerce Website v3/ecommerce_website/core/views.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/core/views.py
@@ -5,7 +5,6 @@ from .forms import RegistrationForm
 from django.contrib.auth.models import User
 
 
-
 def register_user(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -24,21 +23,19 @@ def register_user(request):
 
 def login_user(request):
     if request.method == "POST":
-        email = request.POST["email"]  # Assuming the email field is named "email" in your login form
+        email = request.POST["email"]
         password = request.POST["password"]
-        user = authenticate(username=email, password=password)  # Use email as the username
+        user = authenticate(username=email, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, ('Login successful!'))
-            if 'cart' in request.session:
-                return redirect('checkout')  # Redirect to checkout if cart exists
-            else:
-                return redirect('home')  
+            return redirect('home')  # Redirect to home if cart is empty or doesn't exist
         else:
             messages.error(request, ('Error logging in. Please try again.'))
             return redirect('login')
     else:
         return render(request, 'core/login.html')
+
 
 
 # User Logout  
