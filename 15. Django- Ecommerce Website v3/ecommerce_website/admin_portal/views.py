548f6c5cd6da6bb8cb65f0498ca2d045991398c0
SQCodes <paulsqmagadi@gmail.com>
2024-03-02 10:32:51 +0300

    new file:   15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/__pycache__/__init__.cpython-311.pyc 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/__pycache__/admin.cpython-311.pyc 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/__pycache__/apps.cpython-311.pyc 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/__pycache__/models.cpython-311.pyc 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/migrations/__pycache__/__init__.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/templates/admin_portal/base.html 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/urls.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/views.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/ecommerce_website/__pycache__/settings.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/ecommerce_website/settings.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/ecommerce_website/urls.py 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/static/css/admin_portal/admin_portal.css 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/static/css/admin_portal/navbar.css

--- 15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/views.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/admin_portal/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def admin_portal(request):
+    return render(request, 'admin_portal/admin_portal.html')
--- 15. Django- Ecommerce Website v3/ecommerce_website/ecommerce_website/settings.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/ecommerce_website/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'core',
     'store',
     'cart',
+    'admin_portal',
 ]
 
 MIDDLEWARE = [
