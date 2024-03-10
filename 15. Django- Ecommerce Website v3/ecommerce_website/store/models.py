eb5bef8b0e217c594814925d0f19fafa4364b1c1
SQCodes <paulsqmagadi@gmail.com>
2024-03-10 22:30:20 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/__pycache__/models.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/models.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 import datetime
 from django.utils import timezone
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField()
+    
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     
