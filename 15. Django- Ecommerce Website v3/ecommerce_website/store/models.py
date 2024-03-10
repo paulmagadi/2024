293b0090fd5ff79d93a6857f68841da86a4418c6
SQCodes <paulsqmagadi@gmail.com>
2024-03-10 22:35:43 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/__pycache__/models.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/models.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
@@ -7,7 +7,13 @@ from django.utils import timezone
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_modified = models.DateTimeField(User, auto_now=True)
-    phone = models.CharField()
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    state = models.CharField(max_length=200, blank=True)
+    zipcode = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
     
 
 class Category(models.Model):
