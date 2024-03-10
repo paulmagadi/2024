8b767abae4155eacd6e0846c2ba8e067992e91ff
SQCodes <paulsqmagadi@gmail.com>
2024-03-10 22:49:52 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/__pycache__/admin.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/admin.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/store/admin.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/store/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Product, Category
-
+from .models import Product, Category, Profile
+from django.contrib.auth.models import User
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Profile)
+
+
+#add profile info to user
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+
+    
