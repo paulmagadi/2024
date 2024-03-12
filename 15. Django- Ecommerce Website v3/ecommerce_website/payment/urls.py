aa802c63a7da0cc37c8d0dbaaf4d8865c4cde05e
SQCodes <paulsqmagadi@gmail.com>
2024-03-12 14:14:15 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/payment/__pycache__/urls.cpython-311.pyc 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/payment/templates/payment/payment_fail.html 	new file:   15. Django- Ecommerce Website v3/ecommerce_website/payment/templates/payment/payment_sucess.html 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/payment/urls.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/payment/urls.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/payment/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('payment_sucess/', views.payment_sucess, name='payment_sucess')
+    path('payment_sucess/', views.payment_sucess, name='payment_sucess'),
     
 ]
