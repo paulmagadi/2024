2d211c0bd36eaac2dcc8b0ca976b8e29041031a0
SQCodes <paulsqmagadi@gmail.com>
2024-03-13 17:57:36 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/core/__pycache__/views.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/payment/__pycache__/models.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/payment/__pycache__/views.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/payment/models.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/payment/views.py 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/__pycache__/models.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/store/models.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/store/models.py
@@ -19,6 +19,26 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
     
+
+class ShippingAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    full_name =  models.CharField(max_length=255)
+    email =  models.CharField(max_length=255)
+    address1 =  models.CharField(max_length=255)
+    address2 =  models.CharField(max_length=255 , null=True, blank=True)
+    city =  models.CharField(max_length=255)
+    state = models.CharField(max_length=255, null=True, blank=True)
+    country =  models.CharField(max_length=255)
+    
+    
+    class Meta:
+        verbose_name_plural = "Shipping Address"
+        
+    
+    def __str__(self):
+        return f'Shipping Address - {str(self.id)}'    
+    
+    
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user_profile = Profile(user=instance)
