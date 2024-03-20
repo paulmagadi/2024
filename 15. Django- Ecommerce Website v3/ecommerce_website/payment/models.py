b83dca2e9adb3e52b3ab93cb57dac0953ffba5f7
SQCodes <paulsqmagadi@gmail.com>
2024-03-20 18:32:57 +0300

    modified:   15. Django- Ecommerce Website v3/ecommerce_website/db.sqlite3 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/payment/__pycache__/models.cpython-311.pyc 	modified:   15. Django- Ecommerce Website v3/ecommerce_website/payment/models.py

--- 15. Django- Ecommerce Website v3/ecommerce_website/payment/models.py
+++ 15. Django- Ecommerce Website v3/ecommerce_website/payment/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from store.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blanck=True)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    shipping_address = models.TextField(max_length=15000)
+    amount_paid = models.DecimalField(max_digits=12, decimal_places=2)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Order - {str(self.id)}'
