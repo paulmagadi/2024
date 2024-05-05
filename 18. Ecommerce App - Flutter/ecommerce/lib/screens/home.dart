58f21834ca6e9b658520fe1aa956427afe646aa6
SQCodes <paulsqmagadi@gmail.com>
2024-05-05 16:42:36 +0300

    modified:   18. Ecommerce App - Flutter/ecommerce/lib/screens/home.dart

--- 18. Ecommerce App - Flutter/ecommerce/lib/screens/home.dart
+++ 18. Ecommerce App - Flutter/ecommerce/lib/screens/home.dart
@@ -85,6 +85,24 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ),
+
+          CarouselSlider(
+            options: CarouselOptions(height: 400.0),
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(color: Colors.amber),
+                      child: Text(
+                        'text $i',
+                        style: TextStyle(fontSize: 16.0),
+                      ));
+                },
+              );
+            }).toList(),
+          )
           // Add other sections such as featured products, promotions, etc.
         ],
       ),
