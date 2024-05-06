ea857da6780785acfcdcab6a52df67926ca7ac69
SQCodes <paulsqmagadi@gmail.com>
2024-05-06 18:10:54 +0300

    modified:   18. Ecommerce App - Flutter/ecommerce/lib/screens/deals.dart

--- 18. Ecommerce App - Flutter/ecommerce/lib/screens/deals.dart
+++ 18. Ecommerce App - Flutter/ecommerce/lib/screens/deals.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 
-class DealsScreen extends StatefulWidget {
-  const DealsScreen({super.key});
+class DealsScreen extends StatelessWidget {
+  const DealsScreen({Key? key}) : super(key: key);
 
-  @override
-  State<DealsScreen> createState() => _DealsScreenState();
-}
-
-class _DealsScreenState extends State<DealsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Text("This is Deals Page")
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Deals'),
+      ),
+      body: Center(
+        // Customize the body of the deals screen as needed
+        child: const Text('This is the deals screen'),
       ),
     );
   }
