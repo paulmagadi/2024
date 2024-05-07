2040269f7125d2008038dfa2491705a8f4944a53
SQCodes <paulsqmagadi@gmail.com>
2024-05-07 22:07:40 +0300

    modified:   18. Ecommerce App - Flutter/backend/db.sqlite3 	modified:   18. Ecommerce App - Flutter/backend/ecommerce/settings.py 	new file:   18. Ecommerce App - Flutter/backend/media/uploads/products/footwear-5408643_1280.jpg 	modified:   18. Ecommerce App - Flutter/backend/store/serializers.py 	modified:   18. Ecommerce App - Flutter/ecommerce/android/app/src/debug/AndroidManifest.xml

--- 18. Ecommerce App - Flutter/backend/store/serializers.py
+++ 18. Ecommerce App - Flutter/backend/store/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import Product, Category
 
 class ProductSerializer(serializers.ModelSerializer):
