bc1d83b6e7d160a7a9f32da0991e15e9ec7768b6
SQCodes <paulsqmagadi@gmail.com>
2024-05-07 14:10:25 +0300

    modified:   18. Ecommerce App - Flutter/backend/ecommerce/settings.py 	modified:   18. Ecommerce App - Flutter/backend/store/models.py

--- 18. Ecommerce App - Flutter/backend/ecommerce/settings.py
+++ 18. Ecommerce App - Flutter/backend/ecommerce/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
--- 18. Ecommerce App - Flutter/backend/store/models.py
+++ 18. Ecommerce App - Flutter/backend/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.CharField(max_length=255)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
