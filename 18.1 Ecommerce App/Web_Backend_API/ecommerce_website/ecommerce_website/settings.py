f074b88f478f6c76a1a23cbcee88227ae3139a31
SQCodes <paulsqmagadi@gmail.com>
2024-05-08 12:36:47 +0300

    modified:   18.1 Ecommerce App/Mobile/ecommerce/android/app/src/main/AndroidManifest.xml 	modified:   18.1 Ecommerce App/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/settings.cpython-311.pyc 	modified:   18.1 Ecommerce App/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py

--- 18.1 Ecommerce App/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py
+++ 18.1 Ecommerce App/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'core',
     'store',
     'cart',
@@ -36,6 +37,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +136,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
