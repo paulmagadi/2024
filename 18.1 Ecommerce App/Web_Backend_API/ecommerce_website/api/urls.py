59be64261bfa8e7ea2c8a475872bcd2a41a17dea
SQCodes <paulsqmagadi@gmail.com>
2024-05-08 11:52:53 +0300

    modified:   18.1 Ecommerce App/Web_Backend_API/ecommerce_website/api/urls.py

--- 18.1 Ecommerce App/Web_Backend_API/ecommerce_website/api/urls.py
+++ 18.1 Ecommerce App/Web_Backend_API/ecommerce_website/api/urls.py
@@ -11,5 +11,10 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
 ]
+
+
+
