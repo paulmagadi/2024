5ef4ffaa7d3782a92458dac15c0f7cb72760ed46
SQCodes <paulsqmagadi@gmail.com>
2024-05-09 11:23:37 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/models/product_data.dart 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/__pycache__/models.cpython-311.pyc

--- 18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/models/product_data.dart
+++ 18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/models/product_data.dart
@@ -4,7 +4,7 @@ import 'product_model.dart';
 
 Future<List<Product>> fetchProducts() async {
   final response =
-      await http.get(Uri.parse('http://127.0.0.1:8000/api/products/'));
+      await http.get(Uri.parse('http://10.0.2.2:8000/api/products/'));
 
   if (response.statusCode == 200) {
     List<dynamic> data = jsonDecode(response.body);
