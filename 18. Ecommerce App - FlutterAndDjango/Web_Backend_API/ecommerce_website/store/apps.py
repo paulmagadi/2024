da042ee7571ab4c0fb844392608391871cf2f0c8
SQCodes <paulsqmagadi@gmail.com>
2024-05-11 10:05:55 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/__init__.py 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/__pycache__/__init__.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/__pycache__/apps.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/__pycache__/signals.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/apps.py 	deleted:    18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/signals.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/apps.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
-
 
 
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
-
-    def ready(self):
-        # Import the signal handler function here to avoid import loop issues
-        from .signals import update_is_new
-        
-        # Connect the signal handler function to the post_save signal of the Product model
-        Product = self.get_model('Product')
-        post_save.connect(update_is_new, sender=Product)
