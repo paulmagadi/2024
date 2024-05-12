c758d17c90cbaf222c025f2698f1513d31e74e95
SQCodes <paulsqmagadi@gmail.com>
2024-05-12 23:18:00 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/__pycache__/admin.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/admin.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/admin.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Profile, ShippingAddress, WebBanner, ProductImage
+from .models import Product, Category, Profile, ShippingAddress, WebBanner, ProductImage, Specification
 from django.contrib.auth.models import User
 # Register your models here.
 admin.site.register(Category)
@@ -8,6 +8,7 @@ admin.site.register(Profile)
 admin.site.register(ShippingAddress)
 admin.site.register(WebBanner)
 admin.site.register(ProductImage)
+admin.site.register(Specification)
 
 #add profile info to user
 class ProfileInline(admin.StackedInline):
