14893ae88179d2e3c745ed9f66d571518c0a4888
SQCodes <paulsqmagadi@gmail.com>
2024-05-12 22:56:06 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/__pycache__/models.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/models.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/models.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/models.py
@@ -113,7 +113,7 @@ class Product(models.Model):
     
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
-    image = models.FileField(upload_to='uploads/products/', max_length=12)
+    image = models.FileField(upload_to='uploads/products/', max_length=12, null=True)
     
     class Meta:
         verbose_name_plural = 'Product Images'
