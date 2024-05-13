43312948d8d93fd07dfc3dd5c9becf011218c9fd
SQCodes <paulsqmagadi@gmail.com>
2024-05-13 23:04:11 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/urls.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/urls.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/urls.py
@@ -3,6 +3,25 @@ from rest_framework import routers
 
 from . import views
 
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
@@ -15,7 +34,14 @@ urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='product-list'),
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('mobile_banner/', views.MobileBannerListView.as_view(), name='mobile-banner-list'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+
+
+
 
 
 
