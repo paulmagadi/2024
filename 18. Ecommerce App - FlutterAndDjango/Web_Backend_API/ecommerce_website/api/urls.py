06e7e528903f3b4745713c1cfc0d1ca728ac1221
SQCodes <paulsqmagadi@gmail.com>
2024-05-13 23:19:08 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/__pycache__/urls.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/urls.py 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/urls.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/urls.py
@@ -3,24 +3,7 @@ from rest_framework import routers
 
 from . import views
 
-from django.urls import re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Products API",
-      default_version='v1',
-      description="Backend API",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
-)
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -34,9 +17,6 @@ urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='product-list'),
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('mobile_banner/', views.MobileBannerListView.as_view(), name='mobile-banner-list'),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py
@@ -5,6 +5,25 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from . import settings
 
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Products API",
+      default_version='v1',
+      description="Backend API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
@@ -13,4 +32,7 @@ urlpatterns = [
     path('', include('admin_portal.urls')),
     path('payment/', include('payment.urls')),
     path('api/', include('api.urls')),
+    path('api/documentation', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
