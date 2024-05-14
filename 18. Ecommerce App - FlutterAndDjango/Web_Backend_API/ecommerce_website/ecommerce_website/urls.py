d5ce1793139ef43e511cd610ca7b44ddb43721d8
SQCodes <paulsqmagadi@gmail.com>
2024-05-14 23:42:49 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/__pycache__/views.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/views.py 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/settings.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py
@@ -35,4 +35,6 @@ urlpatterns = [
     # path('api/documentation/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/documentation/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
