4fe8a5add4cadf7590c7da7b123fbc2c5e0a618a
SQCodes <paulsqmagadi@gmail.com>
2024-05-14 23:55:21 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/settings.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py
@@ -31,9 +31,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework', 
-    'djoser',
+    'rest_framework',
     'rest_framework.authtoken',
+    'djoser',
     'corsheaders',
     'drf_yasg',
     'api',
@@ -141,10 +141,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # AUTH_USER_MODEL = 'core.CustomUser'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication'),
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.SessionAuthentication'),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication'
+        ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
 }
 
 DJOSER = {
