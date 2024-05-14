b701960cebc0f5816e7acb69de1f2e5ace8cb45e
SQCodes <paulsqmagadi@gmail.com>
2024-05-14 22:56:46 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/db.sqlite3 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py 	new file:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/migrations/0023_alter_product_category_alter_product_specification_and_more.py 	new file:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/store/migrations/__pycache__/0023_alter_product_category_alter_product_specification_and_more.cpython-311.pyc

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication'),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication'),
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
