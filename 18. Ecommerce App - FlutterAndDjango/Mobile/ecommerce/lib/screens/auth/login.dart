232ebb8771de1673f918cacd8bd6784124556704
SQCodes <paulsqmagadi@gmail.com>
2024-05-15 17:19:21 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/login.dart

--- 18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/login.dart
+++ 18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/login.dart
@@ -25,7 +25,7 @@ class _LoginScreenState extends State<LoginScreen> {
       });
 
       final response = await http.post(
-        Uri.parse('https://10.0.2.2:8000/api/login/'),
+        Uri.parse('http://10.0.2.2:8000/api/login/'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({
           'email': _emailController.text,
