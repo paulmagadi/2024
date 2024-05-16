1c9e76a8fc9beb3c645d3eda544de71d54831ac9
SQCodes <paulsqmagadi@gmail.com>
2024-05-16 13:40:38 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'rest_framework.authtoken',
+    # 'rest_framework.authtoken',
     'djoser',
     'corsheaders',
     'drf_yasg',
--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py
@@ -32,9 +32,10 @@ urlpatterns = [
     path('', include('admin_portal.urls')),
     path('payment/', include('payment.urls')),
     path('api/', include('api.urls')),
-    # path('api/documentation/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('api/documentation/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('auth/', include('djoser.urls')),
-    path('auth/',  include('djoser.urls.authtoken')),
+   #  path('auth/', include('djoser.urls')),
+   #  path('auth/',  include('djoser.urls.authtoken')),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.jwt')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
