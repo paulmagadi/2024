32791acadf21e5016d7a0ea42af1763b9fbae661
SQCodes <paulsqmagadi@gmail.com>
2024-05-16 14:58:43 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py
@@ -10,6 +10,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -38,4 +40,6 @@ urlpatterns = [
    #  path('auth/',  include('djoser.urls.authtoken')),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
