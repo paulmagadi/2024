b2c9a827f34aec2b0c736978d18a4901fb841e8c
SQCodes <paulsqmagadi@gmail.com>
2024-05-17 16:00:10 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/auth_services.dart 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/db.sqlite3 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/__pycache__/urls.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/ecommerce_website/urls.py

--- 18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/auth_services.dart
+++ 18. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/auth_services.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthServices {
-  static const String baseUrl = 'http://10.0.2.2:8000/api/';
+  static const String baseUrl = 'http://10.0.2.2:8000/api/csrf/';
 
   static Future<String?> fetchCsrfToken() async {
     final response = await http.get(Uri.parse('${baseUrl}csrf/'));
