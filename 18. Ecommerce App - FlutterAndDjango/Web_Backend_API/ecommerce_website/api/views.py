93a8c3d62fe04418df41ad5310873f71ef35fb14
SQCodes <paulsqmagadi@gmail.com>
2024-05-17 18:29:50 +0300

    modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/__pycache__/views.cpython-311.pyc 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/views.py 	modified:   18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/db.sqlite3

--- 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/views.py
+++ 18. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/views.py
@@ -15,50 +15,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-# from django.middleware.csrf import get_token
-# from django.http import JsonResponse
 
-# def get_csrf_token(request):
-#     token = get_token(request)
-#     return JsonResponse({'csrfToken': token})
-
-
-# def login(request):
-#     return render(request, 'core/login.html')
-
-# def register(request):
-#     return render(request, 'core/register.html')
-
-# def logout_user(request):
-#     messages.success(request, ('You have been logged out!!!'))
-#     return redirect('home')
-
-
-# @api_view(['POST'])
-# def register(request):
-#     data = request.data
-#     username = data.get('username')
-#     email = data.get('email')
-#     password = data.get('password')
-#     if not username or not email or not password:
-#         return JsonResponse({'error': 'Missing fields'}, status=status.HTTP_400_BAD_REQUEST)
-#     if User.objects.filter(username=username).exists():
-#         return JsonResponse({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
-#     if User.objects.filter(email=email).exists():
-#         return JsonResponse({'error': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
-#     user = User.objects.create_user(username=username, email=email, password=password)
-#     return JsonResponse({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
-
-# @api_view(['POST'])
-# def login(request):
-#     data = request.data
-#     email = data.get('email')
-#     password = data.get('password')
-#     user = authenticate(username=email, password=password)
-#     if user is not None:
-#         return JsonResponse({'message': 'Login successful'}, status=status.HTTP_200_OK)
-#     else:
-#         return JsonResponse({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
     
 
 
