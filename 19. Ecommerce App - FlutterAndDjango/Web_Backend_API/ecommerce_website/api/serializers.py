001d0576586586ec94c927178af0d190bfeee14a
SQCodes <paulsqmagadi@gmail.com>
2024-05-18 13:39:28 +0300

    modified:   19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/__pycache__/serializers.cpython-311.pyc 	modified:   19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py

--- 19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py
+++ 19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py
@@ -2,7 +2,26 @@ from django.contrib.auth.models import Group, User
 from store.models import Product, Category, Specification
 from api.models import MobileBanner
 from rest_framework import serializers
-        
+
+from djoser.serializers import UserCreateSerializer
+from rest_framework import serializers
+from .models import CustomUser  
+
+class CustomUserCreateSerializer(UserCreateSerializer):
+    class Meta(UserCreateSerializer.Meta):
+        model = CustomUser
+        fields = ('id', 'email', 'first_name', 'last_name', 'password')
+
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
