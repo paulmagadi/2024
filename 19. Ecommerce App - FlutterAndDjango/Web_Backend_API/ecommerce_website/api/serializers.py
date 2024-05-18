24c99001f54f4a11253366b280bf6d0bd9e2b255
SQCodes <paulsqmagadi@gmail.com>
2024-05-18 16:33:10 +0300

    modified:   19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/__pycache__/serializers.cpython-311.pyc 	modified:   19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py

--- 19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py
+++ 19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py
@@ -5,21 +5,24 @@ from rest_framework import serializers
 
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
-from core.models import CustomUser  
+from store.models import UserProfile
 
-class CustomUserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = CustomUser
-        fields = ('id', 'email', 'first_name', 'last_name', 'password')
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['first_name', 'last_name']
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = UserProfileSerializer()
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'profile', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = CustomUser(
-            email=validated_data['email'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
-        )
-        user.set_password(validated_data['password'])
-        user.save()
+        profile_data = validated_data.pop('profile')
+        user = User.objects.create_user(**validated_data)
+        UserProfile.objects.create(user=user, **profile_data)
         return user
 
 
