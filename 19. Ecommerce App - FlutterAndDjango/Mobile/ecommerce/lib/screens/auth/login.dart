c1280c37bc633372f2b9820b8512ee63783ccb07
SQCodes <paulsqmagadi@gmail.com>
2024-05-18 12:39:15 +0300

    modified:   19. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/login.dart

--- 19. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/login.dart
+++ 19. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/login.dart
@@ -1,8 +1,9 @@
+import 'package:ecommerce/screens/auth/registration.dart';
+import 'package:ecommerce/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'home.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
