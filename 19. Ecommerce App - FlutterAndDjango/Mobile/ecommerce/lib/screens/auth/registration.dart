6d1730ed7de83c00580fff245ff0c1d9e2acc217
SQCodes <paulsqmagadi@gmail.com>
2024-05-18 13:25:54 +0300

    modified:   19. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/registration.dart

--- 19. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/registration.dart
+++ 19. Ecommerce App - FlutterAndDjango/Mobile/ecommerce/lib/screens/auth/registration.dart
@@ -9,6 +9,8 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   final _formKey = GlobalKey<FormState>();
+  // final TextEditingController _textController = TextEditingController();
+  // final TextEditingController _textController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController = TextEditingController();
