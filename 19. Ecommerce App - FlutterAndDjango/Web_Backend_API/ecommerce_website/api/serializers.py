81bde9d8e4b9fedad2735b15557b682ad6e9bcc7
SQCodes <paulsqmagadi@gmail.com>
2024-05-18 14:55:12 +0300

    modified:   19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/__pycache__/serializers.cpython-311.pyc 	modified:   19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py 	modified:   19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/db.sqlite3

--- 19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py
+++ 19. Ecommerce App - FlutterAndDjango/Web_Backend_API/ecommerce_website/api/serializers.py
@@ -5,15 +5,15 @@ from rest_framework import serializers
 
 from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
-from .models import CustomUser  
+from store.models import Profile  
 
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
-        model = CustomUser
+        model = Profile
         fields = ('id', 'email', 'first_name', 'last_name', 'password')
 
     def create(self, validated_data):
-        user = CustomUser(
+        user = Profile(
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
