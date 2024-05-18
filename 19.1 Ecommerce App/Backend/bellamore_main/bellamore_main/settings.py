210af3e69b3e1d7deea05ea0133621e6162b1fac
SQCodes <paulsqmagadi@gmail.com>
2024-05-18 21:37:19 +0300

    new file:   19.1 Ecommerce App/Backend/bellamore_main/api/__pycache__/__init__.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/bellamore_main/api/__pycache__/serializers.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/bellamore_main/api/__pycache__/urls.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/bellamore_main/api/__pycache__/views.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/bellamore_main/api/serializers.py 	new file:   19.1 Ecommerce App/Backend/bellamore_main/api/urls.py 	modified:   19.1 Ecommerce App/Backend/bellamore_main/api/views.py 	modified:   19.1 Ecommerce App/Backend/bellamore_main/bellamore_main/__pycache__/settings.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/bellamore_main/bellamore_main/__pycache__/urls.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/bellamore_main/bellamore_main/__pycache__/wsgi.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/bellamore_main/bellamore_main/settings.py 	modified:   19.1 Ecommerce App/Backend/bellamore_main/bellamore_main/urls.py 	new file:   19.1 Ecommerce App/Backend/bellamore_main/db.sqlite3

--- 19.1 Ecommerce App/Backend/bellamore_main/bellamore_main/settings.py
+++ 19.1 Ecommerce App/Backend/bellamore_main/bellamore_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,10 @@ SECRET_KEY = 'django-insecure-yzeqf+e&&385urq%4lq(9@#9tawc6xpl$p!h6i4^h43pc-spi=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '10.0.2.2',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://localhost:8080",
 ]
 
 ROOT_URLCONF = 'bellamore_main.urls'
@@ -121,3 +134,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+}
