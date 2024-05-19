1a4c4de147ea635aabc9eab638c80ba1729981dd
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 16:27:48 +0300

    modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/views.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/main/users/templates/users/login.html 	modified:   19.1 Ecommerce App/Backend/main/users/templates/users/register.html 	modified:   19.1 Ecommerce App/Backend/main/users/views.py

--- 19.1 Ecommerce App/Backend/main/users/views.py
+++ 19.1 Ecommerce App/Backend/main/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from .forms import CustomUserRegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 def register(request):
     if request.method == 'POST':
@@ -10,9 +11,30 @@ def register(request):
             user = form.save()
             login(request, user)
             messages.success(request, "Registration successful.")
-            return redirect('home')  # Redirect to a success page or homepage
+            return redirect('home') 
         else:
             messages.error(request, "Unsuccessful registration. Invalid information.")
     else:
         form = CustomUserRegistrationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('home')  
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
