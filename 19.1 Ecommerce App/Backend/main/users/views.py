8dacc6c5c2a1d21d3ff102f310132ceac676d440
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 16:02:08 +0300

    modified:   19.1 Ecommerce App/Backend/main/users/views.py

--- 19.1 Ecommerce App/Backend/main/users/views.py
+++ 19.1 Ecommerce App/Backend/main/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import CustomUserRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect('home')  # Redirect to a success page or homepage
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+    else:
+        form = CustomUserRegistrationForm()
+    return render(request, 'register.html', {'form': form})
