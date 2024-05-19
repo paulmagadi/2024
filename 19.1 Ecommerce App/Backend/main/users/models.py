c298b19fd44b3d4c3d3aa8d3cd77ae4ee9008375
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 14:41:27 +0300

    modified:   19.1 Ecommerce App/Backend/main/users/models.py

--- 19.1 Ecommerce App/Backend/main/users/models.py
+++ 19.1 Ecommerce App/Backend/main/users/models.py
@@ -1,3 +1,20 @@
+from datetime import timezone
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+# from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(verbose_name='email address', unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
