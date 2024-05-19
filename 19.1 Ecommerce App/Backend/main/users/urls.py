877a5d84092194d4435ea82120e8b360223ebb3f
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 22:30:16 +0300

    modified:   19.1 Ecommerce App/Backend/main/core/__pycache__/views.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/core/views.py 	modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/urls.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/main/users/templates/users/shipping_information.html 	new file:   19.1 Ecommerce App/Backend/main/users/templates/users/update_info.html 	new file:   19.1 Ecommerce App/Backend/main/users/templates/users/update_password.html 	new file:   19.1 Ecommerce App/Backend/main/users/templates/users/update_user.html 	new file:   19.1 Ecommerce App/Backend/main/users/templates/users/user_profile.html 	modified:   19.1 Ecommerce App/Backend/main/users/urls.py

--- 19.1 Ecommerce App/Backend/main/users/urls.py
+++ 19.1 Ecommerce App/Backend/main/users/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_user, name='register'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('login_user/', views.login_user, name='login_user'),
+    path('logout_user/', views.logout_user, name='logout_user'),
+    path('user_profile', views.user_profile, name='user_profile'),
+    path('update_user/', views.update_user, name='update_user'),
+    path('update_password/', views.update_password, name='update_password'),
+    path('update_info/', views.update_info, name='update_info'),
+    path('shipping_info/', views.shipping_info, name='shipping_info'),
 ]
