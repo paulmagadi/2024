e5419107f7a476fe9928c4286c7b70e27d56ce4b
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 22:49:15 +0300

    modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/forms.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/users/forms.py

--- 19.1 Ecommerce App/Backend/main/users/forms.py
+++ 19.1 Ecommerce App/Backend/main/users/forms.py
@@ -32,33 +32,6 @@ class CustomUserRegistrationForm(UserCreationForm):
         fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
         
         
-
-
-class RegistrationForm(forms.Form):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
-
-    def clean(self):
-        cleaned_data = super().clean()
-        password = cleaned_data.get('password')
-        confirm_password = cleaned_data.get('confirm_password')
-
-        if password != confirm_password:
-            raise forms.ValidationError("Passwords do not match")
-        
-class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    
-
-
-
-
-        
-
 class UpdateUserForm(UserChangeForm):
     password = None
     class Meta:
