af1408f86800820ee640f1cb2e78844a727e608a
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 22:37:46 +0300

    modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/forms.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/views.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/users/forms.py 	modified:   19.1 Ecommerce App/Backend/main/users/views.py

--- 19.1 Ecommerce App/Backend/main/users/forms.py
+++ 19.1 Ecommerce App/Backend/main/users/forms.py
@@ -2,6 +2,10 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, SetPasswordForm
+
+from store.models import Profile, ShippingAddress
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -26,3 +30,72 @@ class CustomUserRegistrationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
+        
+        
+
+
+class RegistrationForm(forms.Form):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+        
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    
+
+
+
+
+        
+
+class UpdateUserForm(UserChangeForm):
+    password = None
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+        
+        
+class UpdateUserPassword(SetPasswordForm):
+    password = None
+    class Meta:
+        model = User
+        fields = ['new_password1', 'new_password2']
+        
+        
+class UpdateInfoForm(forms.ModelForm):
+    phone = forms.CharField()
+    address1 = forms.CharField()
+    address2 = forms.CharField()
+    city = forms.CharField()
+    state = forms.CharField()
+    zipcode = forms.CharField()
+    country = forms.CharField()
+    
+    class Meta:
+        model = Profile
+        fields = ["phone", "address1", "address2", "city", "state", "zipcode", "country"]
+        
+        
+class ShippingAddressForm(forms.ModelForm):
+    phone = forms.CharField()
+    address1 = forms.CharField()
+    address2 = forms.CharField()
+    city = forms.CharField()
+    state = forms.CharField()
+    zipcode = forms.CharField()
+    country = forms.CharField()
+    
+    class Meta:
+        model = ShippingAddress
+        fields = ["phone", "address1", "address2", "city", "state", "zipcode", "country"]
