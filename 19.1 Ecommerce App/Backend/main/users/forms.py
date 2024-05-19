b159691e8a0aa0884782bf3fb96c749679463e4d
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 16:00:17 +0300

    modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/forms.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/users/forms.py

--- 19.1 Ecommerce App/Backend/main/users/forms.py
+++ 19.1 Ecommerce App/Backend/main/users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
+from django import forms
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -15,3 +16,13 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email",)
+
+
+class CustomUserRegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('first_name', 'last_name', 'email', 'password1', 'password2')
