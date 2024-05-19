e1971d434e14e87f058c4198c9507d8b631d1116
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 16:41:02 +0300

    modified:   19.1 Ecommerce App/Backend/main/db.sqlite3 	modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/urls.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/users/urls.py

--- 19.1 Ecommerce App/Backend/main/users/urls.py
+++ 19.1 Ecommerce App/Backend/main/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
