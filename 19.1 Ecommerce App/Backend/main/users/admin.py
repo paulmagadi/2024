c4b382cee49ccae11e1ef31660d2d6db66d43028
SQCodes <paulsqmagadi@gmail.com>
2024-05-19 15:22:17 +0300

    new file:   19.1 Ecommerce App/Backend/main/users/__pycache__/admin.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/main/users/__pycache__/forms.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/models.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/users/admin.py 	modified:   19.1 Ecommerce App/Backend/main/users/models.py

--- 19.1 Ecommerce App/Backend/main/users/admin.py
+++ 19.1 Ecommerce App/Backend/main/users/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
-
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
@@ -14,18 +12,17 @@ class CustomUserAdmin(UserAdmin):
     fieldsets = (
         (None, {"fields": ("email", "password")}),
         ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'user_type')}),
+        ("Important Dates", {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": (
-                "email", "password1", "password2", "is_staff",
-                "is_active", "groups", "user_permissions"
-            )}
-        ),
+            "fields": ("email", "password1", "password2"),
+        }),
     )
     search_fields = ("email",)
     ordering = ("email",)
-
+    filter_horizontal = ("groups", "user_permissions",)
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- 19.1 Ecommerce App/Backend/main/users/models.py
+++ 19.1 Ecommerce App/Backend/main/users/models.py
@@ -11,6 +11,7 @@ class CustomUser(AbstractBaseUser):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(auto_now_add=True)
+    last_login= models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
