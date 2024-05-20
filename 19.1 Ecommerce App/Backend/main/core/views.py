bfa88448fa96619f0399bb0aaa2529b4b2009614
SQCodes <paulsqmagadi@gmail.com>
2024-05-20 15:56:15 +0300

    modified:   19.1 Ecommerce App/Backend/main/cart/templates/cart/cart.html 	modified:   19.1 Ecommerce App/Backend/main/cart/templates/cart/checkout.html 	modified:   19.1 Ecommerce App/Backend/main/core/__pycache__/views.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/core/views.py 	modified:   19.1 Ecommerce App/Backend/main/db.sqlite3 	modified:   19.1 Ecommerce App/Backend/main/store/__pycache__/urls.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/store/urls.py

--- 19.1 Ecommerce App/Backend/main/core/views.py
+++ 19.1 Ecommerce App/Backend/main/core/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from store.models import Product, Category, WebBanner
+from django.contrib import messages
+import datetime
+from django.db.models import Q
 
 def home(request):
-    return render(request, "core/index.html")
+    all_products = Product.objects.all()
+    products = all_products.filter(is_listed=True)
+    # banners = WebBanner.objects.filter(in_use=True)
+    sale_products = products.filter(is_sale=True)
+    new_products = products.filter(is_new=True)
+    featured_products = products.filter(is_featured=True)
+    context = {
+        'products': products,
+        'sale_products': sale_products,
+        'new_products': new_products,
+        'featured_products': featured_products,
+        # 'banners': banners,
+    }
+    return render(request, 'core/index.html', context)
 
--- 19.1 Ecommerce App/Backend/main/store/urls.py
+++ 19.1 Ecommerce App/Backend/main/store/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('product/<int:pk>', views.product, name='product'),
     path('category/<int:pk>/', views.category, name='category'),
     path('categories/', views.categories, name='categories'),
