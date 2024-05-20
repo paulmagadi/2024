56296142a16c8481b2bee4eff16beaa0d0c0183d
SQCodes <paulsqmagadi@gmail.com>
2024-05-20 22:04:06 +0300

    new file:   19.1 Ecommerce App/Backend/main/cart/__pycache__/context_processors.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/db.sqlite3 	modified:   19.1 Ecommerce App/Backend/main/main/__pycache__/settings.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/main/settings.py 	new file:   19.1 Ecommerce App/Backend/main/store/__pycache__/context_processors.cpython-311.pyc

--- 19.1 Ecommerce App/Backend/main/main/settings.py
+++ 19.1 Ecommerce App/Backend/main/main/settings.py
@@ -69,6 +69,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
+                'store.context_processors.categories',
             ],
         },
     },
