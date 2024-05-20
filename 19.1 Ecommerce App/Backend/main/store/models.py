69a1b160150378652907b91e67d1a523325c92ff
SQCodes <paulsqmagadi@gmail.com>
2024-05-20 16:04:45 +0300

    modified:   19.1 Ecommerce App/Backend/main/store/__pycache__/models.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/store/models.py

--- 19.1 Ecommerce App/Backend/main/store/models.py
+++ 19.1 Ecommerce App/Backend/main/store/models.py
@@ -2,55 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 
 import datetime
-from django.utils import timezone
-
-# from users.models import CustomUser
-
-
-# class Profile(models.Model):
-#     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-#     date_modified = models.DateTimeField(CustomUser, auto_now=True)
-#     phone = models.CharField(max_length=20, blank=True)
-#     address1 = models.CharField(max_length=200, blank=True)
-#     address2 = models.CharField(max_length=200, blank=True)
-#     city = models.CharField(max_length=200, blank=True)
-#     state = models.CharField(max_length=200, blank=True)
-#     zipcode = models.CharField(max_length=200, blank=True)
-#     country = models.CharField(max_length=200, blank=True)
-#     old_cart = models.CharField(max_length=255, blank=True)
-    
-#     def __str__(self):
-#         return self.user.username
-    
-
-# class ShippingAddress(models.Model):
-#     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
-#     full_name =  models.CharField(max_length=255)
-#     email =  models.CharField(max_length=255)
-#     address1 =  models.CharField(max_length=255)
-#     address2 =  models.CharField(max_length=255 , null=True, blank=True)
-#     city =  models.CharField(max_length=255)
-#     state = models.CharField(max_length=255, null=True, blank=True)
-#     country =  models.CharField(max_length=255)
-    
-    
-#     class Meta:
-#         verbose_name_plural = "Shipping Address"
-        
-    
-#     def __str__(self):
-#         return f'Shipping Address - {str(self.id)}'    
-    
-    
-# def create_profile(sender, instance, created, **kwargs):
-#     if created:
-#         user_profile = Profile(user=instance)
-#         user_profile.save()
-        
-# # automate
-# post_save.connect(create_profile, sender=CustomUser)
-
-        
+from django.utils import timezone      
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
