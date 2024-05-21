7f4b8af653b929b01503723187ad11242f3eb2b6
SQCodes <paulsqmagadi@gmail.com>
2024-05-21 11:03:22 +0300

    modified:   19.1 Ecommerce App/Backend/main/api/__pycache__/urls.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/main/api/__pycache__/views.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/api/urls.py 	modified:   19.1 Ecommerce App/Backend/main/api/views.py

--- 19.1 Ecommerce App/Backend/main/api/urls.py
+++ 19.1 Ecommerce App/Backend/main/api/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path, include
+from rest_framework import routers
+
+from . import views
+from .views import get_csrf_token
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('api/csrf/', get_csrf_token, name='api-csrf-token'),
     path('auth/', include('djoser.urls')),
     path('auth/token/', include('djoser.urls.authtoken')),  
 ]
+
+
+
--- 19.1 Ecommerce App/Backend/main/api/views.py
+++ 19.1 Ecommerce App/Backend/main/api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+from django.contrib.auth.models import Group, User
+from django.shortcuts import redirect, render
+from rest_framework import permissions, viewsets, generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import ProductSerializer, SpecificationSerializer, CategorySerializer
+
+from store.models import Product, Category
+# from .models import MobileBanner
+
+
+from django.http import JsonResponse
+from django.middleware.csrf import get_token
+
+def get_csrf_token(request):
+    return JsonResponse({'csrfToken': get_token(request)})
+
+
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny] 
+
+
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.AllowAny] 
+
+# class MobileBannerListView(generics.ListAPIView):
+#     queryset = MobileBanner.objects.all()
+#     serializer_class = MobileBannerSerializer
+#     permission_classes = [permissions.AllowAny] 
