8715e1a3dab0cf3529fbeefe478ae37abda1dc53
SQCodes <paulsqmagadi@gmail.com>
2024-05-21 20:48:52 +0300

    modified:   19.1 Ecommerce App/Backend/main/core/templates/core/include/navbar.html 	modified:   19.1 Ecommerce App/Backend/main/db.sqlite3 	modified:   19.1 Ecommerce App/Backend/main/users/__pycache__/urls.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/users/templates/users/login.html 	modified:   19.1 Ecommerce App/Backend/main/users/templates/users/register.html 	modified:   19.1 Ecommerce App/Backend/main/users/urls.py

--- 19.1 Ecommerce App/Backend/main/users/urls.py
+++ 19.1 Ecommerce App/Backend/main/users/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_user, name='register'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
+    path('login/', views.login_user, name='signin'),
+    path('logout/', views.logout_user, name='signout'),
     path('user_profile', views.user_profile, name='user_profile'),
     path('update_user/', views.update_user, name='update_user'),
     path('update_password/', views.update_password, name='update_password'),
