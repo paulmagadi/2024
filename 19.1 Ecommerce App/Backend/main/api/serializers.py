12dab35b9e3ff827b7571fab8f2f0093f9220aa1
SQCodes <paulsqmagadi@gmail.com>
2024-05-21 10:58:34 +0300

    modified:   19.1 Ecommerce App/Backend/main/api/__pycache__/serializers.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/api/serializers.py

--- 19.1 Ecommerce App/Backend/main/api/serializers.py
+++ 19.1 Ecommerce App/Backend/main/api/serializers.py
@@ -1,8 +1,7 @@
-# users/serializers.py
-
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from users.models import CustomUser
+from store.models import Product, Category, Specification
 
 class CustomUserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
@@ -13,4 +12,26 @@ class CustomUserSerializer(UserSerializer):
     class Meta(UserSerializer.Meta):
         model = CustomUser
         fields = ('id', 'email', 'first_name', 'last_name')
+    
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        
+class SpecificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Specification
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        depth = 1
+
+# class MobileBannerSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = MobileBanner
+#         fields = '__all__'
 
