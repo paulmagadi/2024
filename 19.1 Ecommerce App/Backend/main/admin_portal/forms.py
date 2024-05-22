e940f616516f719198e5058eea34faf6763b0880
SQCodes <paulsqmagadi@gmail.com>
2024-05-22 12:07:31 +0300

    modified:   19.1 Ecommerce App/Backend/main/admin_portal/__pycache__/forms.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/admin_portal/__pycache__/views.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/admin_portal/forms.py 	modified:   19.1 Ecommerce App/Backend/main/admin_portal/views.py 	modified:   19.1 Ecommerce App/Backend/main/db.sqlite3 	modified:   19.1 Ecommerce App/Backend/main/main/__pycache__/settings.cpython-311.pyc 	new file:   19.1 Ecommerce App/Backend/main/media/uploads/products/running-shoe-371624_1280.jpg 	new file:   19.1 Ecommerce App/Backend/main/media/uploads/products/running-shoe-371624_1280_SDraGXP.jpg 	new file:   19.1 Ecommerce App/Backend/main/media/uploads/products/running-shoe-371624_1280_TF81SfZ.jpg

--- 19.1 Ecommerce App/Backend/main/admin_portal/forms.py
+++ 19.1 Ecommerce App/Backend/main/admin_portal/forms.py
@@ -3,32 +3,20 @@ from store.models import Product, Category, ProductImage
 from django.forms.widgets import ClearableFileInput
 
 
-class MultipleFileInput(forms.ClearableFileInput):
-    allow_multiple_selected = True
+# class MultipleFileInput(forms.ClearableFileInput):
+#     allow_multiple_selected = True
 
-class MultipleFileField(forms.FileField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("widget", MultipleFileInput())
-        super().__init__(*args, **kwargs)
+# class MultipleFileField(forms.FileField):
+#     def __init__(self, *args, **kwargs):
+#         kwargs.setdefault("widget", MultipleFileInput())
+#         super().__init__(*args, **kwargs)
 
 
 class ProductModelForm(forms.ModelForm):
-    product_images = forms.FileField(widget=forms.FileField(attrs={'multiple': True}), required=False)
-
     class Meta:
         model = Product
         fields = ['name', 'price', 'category', 'description', 'image', 'is_sale', 'sale_price', 'in_stock', 'stock_quantity', 'is_new', 'is_featured', 'is_listed']
 
-    def save(self, commit=True):
-        product = super().save(commit=False)
-        if commit:
-            product.save()
-
-        if self.cleaned_data.get('product_images'):
-            for image in self.files.getlist('product_images'):
-                ProductImage.objects.create(product=product, image=image)
-
-        return product
     
 class CategoryModelForm(forms.ModelForm):
     class Meta:
