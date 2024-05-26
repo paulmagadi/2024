74563d3c6a9c42f73fcec398431d1a6e6746da16
SQCodes <paulsqmagadi@gmail.com>
2024-05-26 13:40:17 +0300

    modified:   19.1 Ecommerce App/Backend/main/cart/__pycache__/views.cpython-311.pyc 	modified:   19.1 Ecommerce App/Backend/main/cart/views.py 	modified:   19.1 Ecommerce App/Backend/main/db.sqlite3

--- 19.1 Ecommerce App/Backend/main/cart/views.py
+++ 19.1 Ecommerce App/Backend/main/cart/views.py
@@ -36,36 +36,20 @@ def cart_add(request):
         
         cart_quantity = cart.__len__()
         response = JsonResponse({'qty': cart_quantity})
-        # messages.success(request, ('Product added to cart'))
         return response
     
     
 def cart_update(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
-        # Get stuff
         product_id = int(request.POST.get('product_id'))
-        product = Product.objects.get(id=product_id)  # Fetch the Product object
+        product = get_object_or_404(Product, id=product_id) 
         product_qty = int(request.POST.get('product_qty'))
 
-        cart.update(request, product=product, quantity=product_qty)  # Pass the Product object
+        cart.update(request, product=product, quantity=product_qty)  
 
         response = JsonResponse({'qty': product_qty})
-        # messages.success(request, 'Cart updated successfully')
         return response
-
-# def cart_delete(request):
-# 	cart = Cart(request)
-# 	if request.POST.get('action') == 'post':
-# 		# Get stuff
-# 		product_id = int(request.POST.get('product_id'))
-        
-# 		# Call delete Function in Cart
-# 		cart.delete(product=product_id)
-
-# 		response = JsonResponse({'product':product_id})
-# 		messages.info(request, ('Item removed from cart'))
-# 		return response
 
 
 def cart_delete(request):
